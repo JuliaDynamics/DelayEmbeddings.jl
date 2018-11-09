0ef62b879c0a86327deea52e951d2b5a19133c4f
George Datseris <datseris.george@gmail.com>
2018-11-09 13:36:08 +0100

    fix tests namespace

--- test/R_params.jl
+++ test/R_params.jl
@@ -22,38 +22,38 @@ test_value = (val, vmin, vmax) -> @test vmin <= val <= vmax
     ds = Systems.henon()
     data = trajectory(ds,1000;dt=1)
     x = data[:,1]
-    @test estimate_delay(x,"first_zero") <= 2
-    @test estimate_delay(x,"first_min")  <= 2
-    # @test estimate_delay(x,"exp_decay")  <= 2
-    # @test 3 <= estimate_delay(x,"mutual_inf"; k=1) <= 10
-    # @test 3 <= estimate_delay(x,"mutual_inf"; k=10) <= 10
+    @test DelayEmbeddings.estimate_delay(x,"first_zero") <= 2
+    @test DelayEmbeddings.estimate_delay(x,"first_min")  <= 2
+    # @test DelayEmbeddings.estimate_delay(x,"exp_decay")  <= 2
+    # @test 3 <= DelayEmbeddings.estimate_delay(x,"mutual_inf"; k=1) <= 10
+    # @test 3 <= DelayEmbeddings.estimate_delay(x,"mutual_inf"; k=10) <= 10
 
     ds = Systems.roessler(ones(3))
     dt = 0.01
     data = trajectory(ds,200,dt=dt)
     x = data[:,1]
-    @test 1.3 <= estimate_delay(x,"first_zero")*dt <= 1.7
-    @test 2.6 <= estimate_delay(x,"first_min")*dt  <= 3.4
+    @test 1.3 <= DelayEmbeddings.estimate_delay(x,"first_zero")*dt <= 1.7
+    @test 2.6 <= DelayEmbeddings.estimate_delay(x,"first_min")*dt  <= 3.4
 
     dt = 0.1
     data = trajectory(ds,2000,dt=dt)
     x = data[:,1]
-    @test 1.3 <= estimate_delay(x,"first_zero")*dt <= 1.7
-    @test 2.6 <= estimate_delay(x,"first_min")*dt  <= 3.4
-    # @test 1.3 <= estimate_delay(x,"mutual_inf")*dt <= 1.8
-    # @test 1.3 <= estimate_delay(x,"mutual_inf"; maxtau=150, k = 2)*dt <= 1.7
+    @test 1.3 <= DelayEmbeddings.estimate_delay(x,"first_zero")*dt <= 1.7
+    @test 2.6 <= DelayEmbeddings.estimate_delay(x,"first_min")*dt  <= 3.4
+    # @test 1.3 <= DelayEmbeddings.estimate_delay(x,"mutual_inf")*dt <= 1.8
+    # @test 1.3 <= DelayEmbeddings.estimate_delay(x,"mutual_inf"; maxtau=150, k = 2)*dt <= 1.7
 
     # ds = Systems.lorenz()
     # dt = 0.05
     # data = trajectory(ds,1000;dt=dt)
     # x = data[500:end,1]
-    # # @test 0.1 <= estimate_delay(x,"mutual_inf")*dt <= 0.4
-    # @test 0.1 <= estimate_delay(x,"exp_decay")*dt  <= 0.4
+    # # @test 0.1 <= DelayEmbeddings.estimate_delay(x,"mutual_inf")*dt <= 0.4
+    # @test 0.1 <= DelayEmbeddings.estimate_delay(x,"exp_decay")*dt  <= 0.4
     #
     dt = 0.1
     data = trajectory(ds,2000;dt=dt)
     x = data[:,1]
-    # @test 0.1 <= estimate_delay(x,"exp_decay")*dt  <= 0.4
+    # @test 0.1 <= DelayEmbeddings.estimate_delay(x,"exp_decay")*dt  <= 0.4
 end
 
 
@@ -61,8 +61,8 @@ end
     s = sin.(0:0.1:1000)
     τ = 15
     Ds = 1:5
-    E1s = estimate_dimension(s, τ, Ds)
-    E2s = stochastic_indicator(s, τ, Ds)
+    E1s = DelayEmbeddings.estimate_dimension(s, τ, Ds)
+    E2s = DelayEmbeddings.stochastic_indicator(s, τ, Ds)
     @test saturation_point(Ds,E1s; threshold=0.01) == 1
 
     # @test minimum(E2s) < 0.1 # THIS TEST FAILS
@@ -71,8 +71,8 @@ end
     data = trajectory(ds,1000;dt=dt)
     s = data[:,1]
     Ds = 1:5
-    E1s = estimate_dimension(s, τ, Ds)
-    E2s = stochastic_indicator(s, τ, Ds)
+    E1s = DelayEmbeddings.estimate_dimension(s, τ, Ds)
+    E2s = DelayEmbeddings.stochastic_indicator(s, τ, Ds)
     @test saturation_point(Ds,E1s; threshold=0.1) ∈ [2, 3]
     @test minimum(E2s) < 0.3
 
@@ -81,14 +81,14 @@ end
     data = trajectory(ds,500;dt=dt)
     s = data[:,1]
     Ds = 1:5
-    E1s = estimate_dimension(s, τ, Ds)
-    E2s = stochastic_indicator(s, τ, Ds)
+    E1s = DelayEmbeddings.estimate_dimension(s, τ, Ds)
+    E2s = DelayEmbeddings.stochastic_indicator(s, τ, Ds)
     @test saturation_point(Ds,E1s; threshold=0.1) ∈ [2, 3]
 
     # @test minimum(E2s) < 0.1 # THIS TEST FAILS
 
     #Test against random signal
-    E2s = stochastic_indicator(rand(100000), 1, 1:5)
+    E2s = DelayEmbeddings.stochastic_indicator(rand(100000), 1, 1:5)
     @test minimum(E2s) > 0.9
 
 end
