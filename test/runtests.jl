c08543010fe00398431808689a975c777529a5c3
Datseris <datseris.george@gmail.com>
2020-03-22 14:33:49 +0100

    reorg tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,9 @@ ti = time()
 const diffeq = (atol = 1e-9, rtol = 1e-9, maxiters = typemax(Int))
 
 include("dataset_tests.jl")
-include("reconstruction_tests.jl")
-include("R_delay.jl")
-include("R_dimension.jl")
+include("embedding_tests.jl")
+include("delaytime_test.jl")
+include("delaycount_test.jl")
 
 ti = time() - ti
 println("\nTest took total time of:")
