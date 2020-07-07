118b18afe30467b5521d2e051e6a6eafdd1c958a
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2020-07-07 16:59:38 +0200

    Added algorithm description; allowed Dataset or Array input; setup testfile

--- src/garcia_almeida.jl
+++ src/garcia_almeida.jl
@@ -49,12 +49,12 @@ end
 ## core: Garcia-Almeida-method for one arbitrary embedding cycle
 
 """
-    garcia_embedding_cycle(Y,s; kwargs...) → N, d_E1
+    garcia_embedding_cycle(Y,s; kwargs...) → N, d_E1 (`Array`, `Array of Arrays`)
 Performs one embedding cycle according to the method proposed in [^Garcia2005a]
-for a given input `Y`(phase space trajectory or time series in `Dataset`-Format
-and `s` (timeseries as `Array`). Returns the proposed N-Statistic `N` and all
-nearest neighbor distances 'd_E1' for each point of the input phase space
-trajectory `Y`. Note that `Y` is a single time series in case of the first
+for a given input `Y` (phase space trajectory or time series in `Dataset`-Format)
+and `s` (timeseries as `Array` or `Dataset`). Returns the proposed N-Statistic
+`N` and all nearest neighbor distances `d_E1` for each point of the input phase
+space trajectory `Y`. Note that `Y` is a single time series in case of the first
 embedding cycle.
 
 ## Keyword arguments
@@ -62,7 +62,7 @@ embedding cycle.
 * `r = 10`: The threshold, which defines the factor of tolerable stretching for
   the d_E1-statistic (see algorithm description).
 * `T = 1`: The forward time step (in sampling units) in order to compute the
-  d_E2-statistic (see algorithm description). Note that in the paper this is
+  `d_E2`-statistic (see algorithm description). Note that in the paper this is
   not a free parameter and always set to `T=1`.
 * `w = 0`: Theiler window (neighbors in time with index `w` close to the point,
   that are excluded from being true neighbors). `w=0` means to exclude only the
@@ -72,12 +72,64 @@ embedding cycle.
   phase space trajectory `Y`.
 
 ## Description
-tbd
+For a range of possible delay values `τ` from 0:`τ_max` one constructs a
+temporary embedding matrix. That is, one concatenates the input phase space
+trajectory `Y` with the `τ`-lagged input time series `s`. For each point on the
+temporary trajectory one computes its nearest neighbor, which is denoted as the
+`d_E1`-statistic for a specific `τ`. Now one considers the distance between the
+reference point and its nearest neighbor `T` sampling units ahead and calls this
+statistic `d_E2`. [^Garcia2005a] strictly use `T=1`, so they forward each
+reference point and its corresponding nearest neighbor just by one (!) sampling
+unit. Here it is a free parameter.
+
+The `N`-statistic is then the fraction of `d_E2`/`d_E1`-pairs which exceed a
+threshold `r`.
+
+Plotted vs. the considered `τ`-values it is proposed to pick the `τ`-value for
+this embedding cycle as the value, where `N` has its first local minimum.
 
 [^Garcia2005a]: Garcia, S. P., Almeida, J. S. (2005). [Nearest neighbor embedding with different time delays. Physical Review E 71, 037204](https://doi.org/10.1103/PhysRevE.71.037204).
 """
 
-function garcia_embedding_cycle(Y::AbstractDataset, s::AbstractArray; τ_max::Int = 50 , r::Int = 10,
+function garcia_embedding_cycle(Y::Dataset, s::Array; τ_max::Int = 50 , r::Int = 10,
+    T::Int = 1, w::Int = 1, metric = Euclidean())
+
+    # preallocation of output
+    N_stat = zeros(τ_max+1)
+    NN_distances = [AbstractArray[] for i=1, j=1:τ_max+1]
+
+    for (i,τ) in enumerate(0:τ_max)
+        # build temporary embedding matrix Y_temp
+        Y_temp = embed2(Y,s,τ)
+        NN = length(Y_temp)     # length of the temporary phase space vector
+        N = NN-T               # accepted length w.r.t. the time horizon `T
+
+        vtree = KDTree(Y_temp[1:N], metric) # tree for input data
+        # nearest neighbors (d_E1)
+        allNNidxs, d_E1 = all_neighbors(vtree, Y_temp[1:N], 1:N, 1, w)
+        # save d_E1-statistic
+        push!(NN_distances[i],hcat(d_E1...))
+
+        # for each point, consider its next iterate and compute the distance to
+        # its former(!) nearest neighbour iterate
+        newfididxs = [j .+ T for j = 1:N]
+        newNNidxs = [j .+ T for j in allNNidxs]
+
+        # compute d_E2
+        d_E2 = [evaluate(metric,Y_temp[newfididxs[i]],Y_temp[newNNidxs[i][1]]) for i = 1:N]
+
+        # look at ratio d_E2/d_E1
+        distance_ratios = d_E2./vcat(d_E1...)
+
+        # number of distance ratio larger than r
+        N_stat[i] = sum(distance_ratios.>r)/NN
+
+    end
+
+    return N_stat, NN_distances
+end
+
+function garcia_embedding_cycle(Y::Dataset, s::Dataset; τ_max::Int = 50 , r::Int = 10,
     T::Int = 1, w::Int = 1, metric = Euclidean())
 
     # preallocation of output
@@ -116,16 +168,37 @@ function garcia_embedding_cycle(Y::AbstractDataset, s::AbstractArray; τ_max::In
 end
 
 
+
 """
     embed2(Y,s,τ) → Dataset
-takes a phase space trajectory 'Y' containing all phase space vectors, a
-univariate time series 's' and a delay value 'τ' as input. embed2 then expands
-the input phase space vectors by an additional component consisting of the
-τ-shifted values of the input time series `s` and outputs the new phase space
-trajectory `Y_new`.
+takes a phase space trajectory 'Y' (`Dataset`) containing all phase space
+vectors, a univariate time series 's' (`Array` or `Dataset`) and a delay value
+'τ' as input. embed2 then expands the input phase space vectors by an additional
+component consisting of the τ-shifted values of the input time series `s` and
+outputs the new phase space trajectory `Y_new` (`Dataset`).
 """
 
-function embed2(Y,s,τ::Int)
+function embed2(Y::Dataset,s::Array,τ::Int)
+    N = size(Y,1)   # length of input trajectory
+    NN = size(Y,2)  # dimensionality of input trajectory
+    M = N - τ       # length of output trajectory
+
+    # preallocation
+    Y_new = zeros(M,NN+1)
+    # fill vector up until index M
+    for i = 1:NN
+        Y_new[:,i] = Y[1:M,i]
+    end
+    # add lagged component of s
+    Y_new[:,NN+1] = s[1+τ:N]
+
+    return Dataset(Y_new)
+
+end
+
+function embed2(Y::Dataset,s::Dataset,τ::Int)
+
+    s = Matrix(s)   # convert Dataset into Array
     N = size(Y,1)   # length of input trajectory
     NN = size(Y,2)  # dimensionality of input trajectory
     M = N - τ       # length of output trajectory
--- test/test_garcia.jl
+++ test/test_garcia.jl
@@ -2,44 +2,49 @@
 using DynamicalSystemsBase
 using DelayEmbeddings
 using StatsBase
-using DelimitedFiles
-using Revise
+using Statistics
 using Random
-using Plots
-# using PyPlot
-#
-# pygui(true)
+using Test
+using Peaks
 
-## Simple Check on Lorenz System
+println("\nTesting garcia_almeida.jl...")
+
+
+## Check on Lorenz System
 lo = Systems.lorenz()
 
 tr = trajectory(lo, 60; dt = 0.01, Ttr = 10)
 
-#writedlm("lorenz_trajectory.csv", tr)
 x = tr[:, 1]
 Y = Dataset(x)
 
 τ_max = 50
 
 @time begin
-N , NN_distances = DelayEmbeddings.garcia_embedding_cycle(Y, x, w=0, T=1, τ_max = τ_max)
+N , NN_distances = garcia_embedding_cycle(Y, x, w=0, T=1, τ_max = τ_max)
 end
 
 T = 17
 @time begin
-N2 , NN_distances2 = DelayEmbeddings.garcia_embedding_cycle(Y, x, w=0, T=T, τ_max = τ_max)
+N2 , NN_distances2 = garcia_embedding_cycle(Y, x, w=0, T=T, τ_max = τ_max)
 end
 
-plot(N,linewidth = 2, label = "T=1 (as in the Paper)", xaxis=:log)
-plot!(N2,linewidth = 2, label = "T=$T", xaxis=:log)
-plot!(title = "Lorenz System as in Fig. 2(a) in the Paper")
-xlabel!("τ")
-ylabel!("N-Statistic")
+# check whether the `d_E1`-statistic is the same
+@test NN_distances[1][1][1:100] == NN_distances2[1][1][1:100]
+@test NN_distances[5][1][1:100] == NN_distances2[5][1][1:100]
 
+min_dist = 4
+max_1_idx = Peaks.maxima(N,min_dist)
+max_2_idx = Peaks.maxima(N2,min_dist)
 
-# ts = readdlm("lorenz_matlab.txt")
-#
-# Y = Dataset(ts)
-# @time begin
-# N , NN_dkistances = DelayEmbeddings.garcia_embedding_cycle(Y, ts, w=0, T=1)
-# end
+@test max_1_idx == max_2_idx
+
+# # plot N-Statistic for the Lorenz system as in Fig. 2(a) in [^Garcia2005b]
+# using Plots
+# plot(N,linewidth = 2, label = "T=1 (as in the Paper)", xaxis=:log)
+# plot!(N2,linewidth = 2, label = "T=$T", xaxis=:log)
+# plot!(N,seriestype = :scatter, label = "",xaxis=:log)
+# plot!(N2,seriestype = :scatter, label = "",xaxis=:log)
+# plot!(title = "Lorenz System as in Fig. 2(a) in the Paper")
+# xlabel!("τ")
+# ylabel!("N-Statistic")
