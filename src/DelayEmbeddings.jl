f0e641c3d82f473c60e1061e1668e8c898c62d76
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2020-07-08 16:44:48 +0200

    init MDOP method files

--- src/DelayEmbeddings.jl
+++ src/DelayEmbeddings.jl
@@ -12,5 +12,6 @@ include("neighborhoods.jl")
 include("estimate_delay.jl")
 include("estimate_dimension.jl")
 include("pecora.jl")
+include("MDOP.jl")
 
 end
---
+++ src/MDOP.jl
@@ -0,0 +1,2 @@
+using Distances, Statistics, StatsBase
+export pecora
