05754055b68c0c4c7a808d6811ee2daca1c9d7c9
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2020-07-29 15:13:25 +0200

    input user-tau-range implemented; output now Vector-type; adjusted tests

--- test/test_MDOP.jl
+++ test/test_MDOP.jl
@@ -5,7 +5,11 @@ using StatsBase
 using Statistics
 using Random
 using Test
-using DelayDiffEq
+using Peaks
+using DelimitedFiles
+using DifferentialEquations
+
+using Revise
 
 println("\nTesting MDOP.jl...")
 
@@ -36,33 +40,55 @@ sol = solve(prob,alg; adaptive=false, dt=δt)
 
 s = sol.u
 s = s[4001:end]
+ss = zeros(length(s))
+[ss[i] = s[i][1] for i in 1:length(s)]
 
 Y = Dataset(s)
-ss = Dataset(s)
 
 # theiler window
 theiler = 57
-tau_max = 100
+taus = 0:100
 
-β = DelayEmbeddings.beta_statistic(Y, s; τ_max = tau_max , w = theiler)
-β2 = DelayEmbeddings.beta_statistic(Y, ss; τ_max = tau_max , w = theiler)
+β = DelayEmbeddings.beta_statistic(Y, ss; τs = taus, w = theiler)
 
 maxi, max_idx = findmax(β)
+
 @test maxi>4.1
-@test max_idx[2]-1>=50
+@test taus[max_idx]>=50
+
+# # display results as in Fig. 3 of the paper
+# using Plots
+# plot(taus, β, linewidth = 3, label = "1st embedding cycle")
+# plot!(title = "β-statistic for Mackey Glass System as in Fig. 3 in the Paper")
+# xlabel!("τ")
+# ylabel!("β-Statistic")
 
 # # display results as in Fig. 3 of the paper
 # using PyPlot
 # pygui(true)
 # figure()
-# plot(0:tau_max, β[1:101], linewidth = 3, label = "1st embedding cycle")
-# scatter(max_idx[2]-1,maxi, c="red")
+# plot(taus, β, linewidth = 3, label = "1st embedding cycle")
+# scatter(max_idx-1,maxi, c="red")
 # title("β-statistic for Mackey Glass System as in Fig. 3 in the Paper")
 # xlabel("τ")
 # ylabel("β-Statistic")
 # xticks(0:10:100)
 # grid()
 
+# test different tau range
+taus2 = 1:4:100
+β2 = DelayEmbeddings.beta_statistic(Y, ss; τs = taus2, w = theiler)
 
+maxi2, max_idx2 = findmax(β2)
+
+@test maxi2>4.1
+@test taus2[max_idx2]>=40
+
+# # display results as in Fig. 3 of the paper
+# using Plots
+# plot(taus2, β2, linewidth = 3, label = "1st embedding cycle")
+# plot!(title = "coarse β-statistic for Mackey Glass System as in Fig. 3 in the Paper")
+# xlabel!("τ")
+# ylabel!("β-Statistic")
 
 # test MDOP()
