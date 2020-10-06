78f5083453ee58f36917882dc31681c42e238654
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2020-10-06 08:11:43 +0200

    cleared docstring

--- src/traditional_de/automated.jl
+++ src/traditional_de/automated.jl
@@ -46,53 +46,6 @@ Cao's method (eqs. (2, 3) of [1]). Defaults to `Euclidean()` (note that [1] used
 
 [^Hegger1999]: Hegger & Kantz, [Improved false nearest neighbor method to detect determinism in time series data. Physical Review E 60, 4970](https://doi.org/10.1103/PhysRevE.60.4970).
 """
-function optimal_traditional_de(s::AbstractVector, method::String = "ifnn", ;
-    delay::String = "mi_min", τs = 1:min(100, length(s)), kwargs...)
-
-Compute the reconstructed trajectory `Y` from a time series `s` using the
-standard time delay embedding. You can choose various combinations of the delay
-estimators `delaymethod` (Default is "mi_min") and embedding dimension estimators
-(Default is "afnn").
-
-You can choose delay estimators from [`estimate_delay`](@ref):
-* `"mi_min"` : delay of first minimum of mutual information of `s` with itself
-  (shifted for various `τs`). <- Default
-* `"ac_zero"` : first delay at which the auto-correlation function becomes <0.
-* `"ac_min"` : delay of first minimum of the auto-correlation function.
-
-You can choose embedding estimators from [`estimate_dimension`](@ref):
-* `"afnn"` : Cao's method [`afnn`](@ref). <- Default
-* `"fnn"`  : Kennel et al.'s traditional method [`fnn`](@ref)
-* `"ifnn"` : improved fnn-method by Hegger & Kantz [`ifnn`](@ref)
-* `"f1nn"` : delay of first minimum of the auto-correlation function.
-
-Keyword arguments:
-*`thres = 0.05`: This threshold determines the tolerable deviation of the
-    proposed statistic from the optimal value of 1 (in case of "afnn") or 0 (in
-    case of the other dimension estimators), for breaking the algorithm.
-*`dmax = 10`: The maximum embedding dimension, which is encountered by the
-    algorithm.
-*`w = 1`: The Theiler window, which excludes temporally correlated points from
-    the nearest neighbor search (does only work for "afnn", yet).
-*`rtol = 10.`: threshold for the relative increment of the distance between the
-    nearest neighbors in case of "fnn"
-*`atol = 2.`: another threshold for the ratio between the increased distance and
-    the "size of the attractor" in case of "fnn"
-*`r = 2`:  Obligatory threshold, which determines the maximum tolerable spreading
-    of trajectories in the reconstruction space in case of "ifnn".
-
-## Description
-Given the scalar timeseries `s` and the requested delay and dimension estimator
-methods, this function returns the reconstruction vectors `Y`, given the threshold
-`thres`. When this threshold is exceeded, the algorithm breaks. In case of "afnn"
-also the [`stochastic_indicator`](@ref) gets checked. Also returns the picked
-delay `τ` and the statistic `d_statistic`, according to the chosen dimension
-estimation method.
-
-*Note that `thres` needs to be adapted in case of noise contamination of the
-input data, otherwise it will not terminate/ give a valid embedding*
-
-"""
 function optimal_traditional_de(s::AbstractVector, delaymethod::String= "mi_min",
     dimensionmethod::String = "afnn"; thres::Real = 0.05, dmax::Int = 10,
     w::Int =1, rtol=10.0, atol=2.0, )
