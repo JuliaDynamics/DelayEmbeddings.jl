3dbed429cf6f39ef71697d0eed963c4fa9b1c1e8
K. Hauke Kraemer <hkraemer@pik-potsdam.de>
2020-10-19 20:41:12 +0200

    updated automated_traditional_de and according test file

--- src/traditional_de/automated.jl
+++ src/traditional_de/automated.jl
@@ -4,22 +4,23 @@ export optimal_traditional_de
     optimal_traditional_de(s, dmethod = "mi_min", method = "afnn"; kwargs...) → 𝒟, τ, x
 
 Produce an optimal delay embedding `𝒟` of the given timeseries `s` by
-using the traditional approach of first finding an optimal (and constant) delay time using
-[`estimate_delay`](@ref) with the given `dmethod`, and then an optimal embedding dimension.
-Return the embedding `𝒟` and the optimal delay time `τ` (the optimal embedding dimension `d`
-is just `size(𝒟, 2)`) and the actual statistic `x` used to estimate optimal `d`.
+using the traditional approach of first finding an optimal (and constant) delay
+time using [`estimate_delay`](@ref) with the given `dmethod`, and then an optimal
+embedding dimension. Return the embedding `𝒟` and the optimal delay time `τ`
+(the optimal embedding dimension `d` is just `size(𝒟, 2)`) and the actual
+statistic `x` used to estimate optimal `d`.
 
 For estimating the dimension we use the given `method`, which can be:
 
-* `"afnn"` (default) is Cao's "Averaged False Nearest Neighbors" method[^Cao1997], which
-    gives a ratio of distances between nearest neighbors.
+* `"afnn"` (default) is Cao's "Averaged False Nearest Neighbors" method[^Cao1997],
+    which gives a ratio of distances between nearest neighbors.
 * `"ifnn"` is the "Improved False Nearest Neighbors" from Hegger & Kantz[^Hegger1999],
     which gives the fraction of false nearest neighbors.
-* `"fnn"` is Kennel's "False Nearest Neighbors" method[^Kennel1992], which gives the
-    number of points that cease to be "nearest neighbors" when the dimension
+* `"fnn"` is Kennel's "False Nearest Neighbors" method[^Kennel1992], which gives
+    the number of points that cease to be "nearest neighbors" when the dimension
     increases.
-* `"f1nn"` is Krakovská's "False First Nearest Neighbors" method[^Krakovská2015], which
-    gives the ratio of pairs of points that cease to be "nearest neighbors"
+* `"f1nn"` is Krakovská's "False First Nearest Neighbors" method[^Krakovská2015],
+    which gives the ratio of pairs of points that cease to be "nearest neighbors"
     when the dimension increases. This is the worse method.
 
 For more details, see individual methods: [`afnn`](@ref), [`ifnn`](@ref),
@@ -28,9 +29,23 @@ For more details, see individual methods: [`afnn`](@ref), [`ifnn`](@ref),
 ## Keywords
 All keywords are propagated to the low level functions like `afnn` (except `τs`).
 ```
-thres::Real = 0.05, dmax::Int = 10, w::Int=1, rtol=10.0, atol=2.0, τs = 1:100,
-metric = Euclidean(), r::Real=2.0
+fnn_thres::Real = 0.05, slope_thres::Real= 0.2, dmax::Int = 10, w::Int=1,
+rtol=10.0, atol=2.0, τs = 1:100, metric = Euclidean(), r::Real=2.0
 ```
+
+## Description
+We estimate the optimal embedding dimension based on the given delay time gained
+from `dmethod` as follows: For Cao's method the optimal dimension is reached,
+when the slope of the `E₁`-statistic (output from `"afnn"`) falls below the
+threshold `slope_thres` (Default is .05) and the according stochastic test turns
+out to be false, i.e. when the `E₂`-statistic is not "equal" to 1 for all en-
+countered dimensions. The treat `E₂`-values as equal to 1, when `1-E₂ ≤ fnn_thres`.
+For all the other methods we return the optimal embedding dimension
+when the corresponding FNN-statistic (output from `"ifnn"`, `"fnn"` or `"f1nn"`)
+falls below the fnn-threshold `fnn_thres` (Default is .05) AND the slope of the
+statistic falls below the threshold `slope_thres`. Note that with noise
+contaminated time series, one might need to adjust `fnn_thres` according to the
+noise level.
 
 [^Cao1997]: Liangyue Cao, [Physica D, pp. 43-50 (1997)](https://www.sciencedirect.com/science/article/pii/S0167278997001188?via%3Dihub)
 
@@ -42,8 +57,8 @@ metric = Euclidean(), r::Real=2.0
 """
 function optimal_traditional_de(s::AbstractVector, delaymethod::String= "mi_min",
         dimensionmethod::String = "afnn";
-        thres::Real = 0.05, dmax::Int = 10, w::Int=1, rtol=10.0, atol=2.0, τs = 1:100,
-        metric = Euclidean(), r::Real=2.0
+        fnn_thres::Real = 0.05, slope_thres::Real = .05, dmax::Int = 10, w::Int=1,
+        rtol=10.0, atol=2.0, τs = 1:100, metric = Euclidean(), r::Real=2.0
     )
 
     @assert dimensionmethod ∈ ("afnn", "fnn", "ifnn", "f1nn")
@@ -52,19 +67,19 @@ function optimal_traditional_de(s::AbstractVector, delaymethod::String= "mi_min"
 
     if dimensionmethod=="afnn"
         dimension_statistic = afnn(s, τ, γs, metric)
-        Y, τ = cao_embed(s, τ, dimension_statistic, thres)
+        Y, τ = cao_embed(s, τ, dimension_statistic, slope_thres)
         E2 = stochastic_indicator(s, τ, γs)
-        flag = is_stochastic(E2, thres)
+        flag = is_stochastic(E2, fnn_thres)
         flag && println("Stochastic signal, valid embedding NOT achieved ⨉.")
     elseif dimensionmethod=="fnn"
         dimension_statistic = fnn(s, τ, γs; rtol, atol)
-        Y, τ = fnn_embed(s, τ, dimension_statistic, thres)
+        Y, τ = fnn_embed(s, τ, dimension_statistic, fnn_thres, slope_thres)
     elseif dimensionmethod=="ifnn"
         dimension_statistic = ifnn(s, τ, γs; r, w, metric)
-        Y, τ = fnn_embed(s, τ, dimension_statistic, thres)
+        Y, τ = fnn_embed(s, τ, dimension_statistic, fnn_thres, slope_thres)
     elseif dimensionmethod=="f1nn"
         dimension_statistic = f1nn(s, τ, γs, metric)
-        Y, τ = fnn_embed(s, τ, dimension_statistic, thres)
+        Y, τ = fnn_embed(s, τ, dimension_statistic, fnn_thres, slope_thres)
     end
     return Y, τ, dimension_statistic
 end
@@ -74,21 +89,23 @@ end
 Helper function for selecting the appropriate embedding dimension from the
 statistic when using Kennel's, Hegger's or Krakovskas's method.
 """
-function fnn_embed(s::Vector{T}, τ::Int, rat::Vector, thres::Real) where {T}
+function fnn_embed(s::Vector{T}, τ::Int, rat::Vector, fnn_thres::Real,
+                                                    slope_thres::Real) where {T}
     @assert length(rat) > 1
+    y = abs.(diff(rat))
     flag = false
     m, Y = 0, nothing
     for i = 2:length(rat)
-        if rat[i] ≤ thres
+        if rat[i] ≤ fnn_thres && y[i-1] ≤ slope_thres
             m = i
             break
         elseif rat[i] > rat[i-1]
             flag = true
-            m = i
+            m = i-1
             break
         end
     end
-    if m == length(rat)
+    if m == length(rat) || m == 0
         Y = s
         println("Sufficiently small FNNs NOT reached."*
                 "Valid embedding NOT achieved ⨉.")
@@ -111,13 +128,14 @@ statistic when using Cao's method.
 """
 function cao_embed(s::Vector{T}, τ::Int, rat::Vector, thres::Real) where {T}
     m, Y = 0, nothing
-    for i = 1:length(rat)
-        if abs(1-rat[i]) < thres
+    y = abs.(diff(rat))
+    for i = 1:length(rat)-1
+        if y[i] ≤ thres
             m = i
             break
         end
     end
-    if m == length(rat)
+    if m == 0
         Y = s
         println("NO convergence of E₁-statistic."*
                 "Valid embedding NOT achieved ⨉.")
@@ -136,7 +154,7 @@ stochastic or not.
 function is_stochastic(rat::Vector{T}, thres::Real) where {T}
     cnt = 0
     for i = 1:length(rat)
-        if abs(1-rat[i]) > thres
+        if abs(1-rat[i]) ≥ thres
             cnt += 1
         end
     end
--- test/compare_different_dimension_estimations.jl
+++ test/compare_different_dimension_estimations.jl
@@ -1,50 +1,76 @@
-using DelayEmbeddings, DynamicalSystemsBase, PyPlot
-
+using DelayEmbeddings, DynamicalSystemsBase, PyPlot, Test
 pygui(true)
-# Generate the first timeseries (use a bunch of different systems)
-#ds = Systems.lorenz96(5; F = 8.0)
-ds = Systems.lorenz()
-tr = trajectory(ds, 1000.0; Ttr = 100.0, dt = 0.05)
-s = tr[:, 1]
-τ = estimate_delay(s, "mi_min")
 
-# do a plot of the differently found methods
-dmax = 10
+function ar_process(u0::T, α::T, p::T, N::Int) where {T<:Real}
+    x = zeros(T, N+10)
+    x[1] = u0
+    for i = 2:N+10
+        x[i] = α*x[i-1] + p*randn()
+    end
+    return x[11:end]
+end
+
+trajectories = Dict(
+    "White noise" => Dataset(randn(5000)),
+    "AR(1)" => Dataset(ar_process(.2,.9,.1,5000)),
+    "Hénon (chaotic)" => trajectory(Systems.henon(a=1.4, b=0.3), 199, Ttr=1000),
+    "Hénon (periodic)" => trajectory(Systems.henon(a=1.054, b=0.3), 199, Ttr=1000),
+    "Lorenz" => trajectory(Systems.lorenz(), 1000.0; Ttr=1000, dt = 0.05),
+    "Lorenz96" => trajectory(Systems.lorenz96(5; F = 8.0), 1000.0; Ttr=1000, dt = 0.05),
+    "Roessler" => trajectory(Systems.roessler(), 1000.0; Ttr=1000, dt = 0.05),
+    "Sine wave" => Dataset(map(x->[sin.(x) cos.(x)], StepRangeLen(0.0,0.2,200)))
+)
+dict_keys = ["White noise","AR(1)","Hénon (chaotic)","Hénon (periodic)","Lorenz","Lorenz96","Roessler","Sine wave"]
+
+# set noise level
+σ = 0.01
+# set maximum encountered dimension
+dmax = 15
 γs = 0:dmax-1
 dims = γs .+ 1
 
-fs = (afnn, fnn, f1nn, ifnn)
-fig, axs  = subplots(2, 1; sharex = true)
-for (i, f) in enumerate(fs)
-    x = f(s, τ, γs)
-    @show f, x
-    if f == afnn
-        x .= 1 .- x
-    end
-    axs[1].plot(dims, x, label = string(f); color = "C$(i)")
-    axs[2].plot(dims[1:end-1], diff(x); color = "C$(i)")
-    # lrs, tans = linear_regions(dims, x, tol = 0.5)
-    # axs[1].scatter(dims[lrs], x[lrs], color = "C$(i)")
-    z = 0.2
-    axs[2].axhspan(-z, z; color = "0.5", alpha = 0.2, label = "±$(z)")
+slope_thres = .1
 
-    # Estimations of condition for convergence:
-    # earliest point where x is both less than a thresohold, 0.2, and local slope is
-    # between ± 0.2
-    z = 0.2 # threshold of slopes
-    r = 0.2 # threshold of convergence near 0
-    y = diff(x)
-    k = 0
-    for j in 1:length(x)-1
-        k = j
-        x[k] < r && y[k] < z && break
+for names in dict_keys
+    tr = trajectories[names]
+    tr = regularize(tr)
+    s = tr[:, 1].+ σ.*randn(length(tr))
+    τ = estimate_delay(s, "mi_min")
+
+    # do a plot of the differently found methods
+
+    fs = (afnn, fnn, f1nn, ifnn)
+    methods = ["afnn", "fnn", "f1nn", "ifnn"]
+    fig, axs  = subplots(2, 1; sharex = true)
+    for (i, f) in enumerate(fs)
+        x = f(s, τ, γs)
+
+        if i == 1
+            x .= 1 .- x
+            E2 = stochastic_indicator(s, τ, γs)
+        end
+
+        axs[1].plot(dims, x, label = string(f); color = "C$(i)")
+        axs[2].plot(dims[1:end-1], diff(x); color = "C$(i)")
+        # lrs, tans = linear_regions(dims, x, tol = 0.5)
+        # axs[1].scatter(dims[lrs], x[lrs], color = "C$(i)")
+        z = slope_thres # threshold of slopes
+        axs[2].axhspan(-z, z; color = "0.5", alpha = 0.2, label = "±$(z)")
+
+        # Estimations of condition for convergence:
+        # earliest point where x is both less than a thresohold, 0.05, and local slope is
+        # between ± 0.05
+        r = 0.05 # threshold of convergence near 0
+        d_max = dmax
+        Y, _, rat =  optimal_traditional_de(s, "mi_min", methods[i]; dmax = d_max, slope_thres = slope_thres)
+        println("$(methods[i]) embedding dimension for $names: $(size(Y,2)) ")
+        axs[1].scatter(dims[size(Y,2)], x[size(Y,2)]; color = "C$(i)", edgecolors  = "k")
     end
-    axs[1].scatter(dims[k], x[k]; color = "C$(i)", edgecolors  = "k")
+    axs[1].grid(); axs[2].grid()
+    axs[1].legend(loc = "upper right", ncol = 2)
+    axs[1].set_ylabel("estimator")
+    axs[2].set_xlabel("dimension")
+    axs[2].set_ylabel("local slope")
+    axs[1].set_title(names)
+
 end
-axs[1].grid(); axs[2].grid()
-axs[1].legend(loc = "upper right", ncol = 2)
-axs[1].set_ylabel("estimator")
-axs[2].set_xlabel("dimension")
-axs[2].set_ylabel("local slope")
-
-axs[1].set_title("5-dim Lorenz96")
