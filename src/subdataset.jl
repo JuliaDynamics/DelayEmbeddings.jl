3d3cb92bc66f9a74aa317908047645ba8902313b
Helios <helios.derosario@gmail.com>
2020-12-07 11:57:53 +0100

    implement SubDataset

---
+++ src/subdataset.jl
@@ -0,0 +1,41 @@
+"""
+    SubDataset{D, T, <:AbstractDataset{D,T}, <:SubArray{<:SVector{D,T},1}} <: AbstractDataset{D,T}
+
+A view of an `AbstractDataset`, as returned by the `view` function
+or the `@view` macro. A `SubDataset` is an `AbstractDataset`
+of the same type as its parent, so indexing and most functions
+can be expected to work in the same way for both the parent and the view.
+"""
+struct SubDataset{D, T, P<:AbstractDataset{D,T}, S<:SubArray{<:SVector{D,T},1}} <: AbstractDataset{D,T}
+    parent::P
+    data::S
+    function SubDataset(par, data)
+        @assert parent(data) === par.data
+        P = typeof(par)
+        S = typeof(data)
+        SV = eltype(P)
+        T = eltype(SV)
+        D = length(SV)
+        new{D,T,P,S}(par, data)
+    end
+end
+
+function Base.summary(sd::SubDataset{D, T}) where {D, T}
+    N = length(sd)
+    return "$N-element view of $D-dimensional Dataset{$(T)}"
+end
+
+Base.parent(sd::SubDataset) = sd.parent
+Base.parentindices(sd::SubDataset) = parentindices(sd.data)
+
+"""
+    view(d::Dataset, indices)
+
+Return a view into the parent dataset `d`, as a [`SubDataset`](@ref)
+that contains the datapoints of `d` referred to by `indices`.
+"""
+Base.view(d::AbstractDataset, i) = SubDataset(d, view(d.data, i))
+
+function Base.view(::AbstractDataset, ::Any, ::Any, ::Vararg)
+    throw(ArgumentError("Dataset views only accept indices on one dimension"))
+end
--- test/dataset_tests.jl
+++ test/dataset_tests.jl
@@ -27,6 +27,10 @@ println("\nTesting Dataset...")
     @test size(data[1:10,1:2]) == (10,2)
     @test data[1:10,1:2] == Dataset(a[1:10], b[1:10])
     @test data[SVector{10}(1:10), SVector(1, 2)] == data[1:10, 1:2]
+
+    sub = @view data[11:20]
+    @test sub isa SubDataset
+    @test sub[2] == data[12]
   end
 
   @testset "minmax" begin
