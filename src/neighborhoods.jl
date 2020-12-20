9aea475d22f2e5b244b1073f8a071fd216226b7b
Datseris <datseris.george@gmail.com>
2020-12-20 16:16:24 +0100

    add a second convenience `all_neighbors` function

--- src/neighborhoods.jl
+++ src/neighborhoods.jl
@@ -4,7 +4,7 @@
 using Neighborhood, Distances
 
 export WithinRange, NeighborNumber
-export Euclidean, Chebyshev, Cityblock
+export Euclidean, Chebyshev, Cityblock, all_neighbors
 
 Neighborhood.KDTree(D::AbstractDataset, metric::Metric = Euclidean(); kwargs...) =
 KDTree(D.data, metric; kwargs...)
@@ -17,18 +17,33 @@ for f ∈ (:bulkisearch, :bulksearch)
     end
 end
 
-"""
+#=
     all_neighbors(vtree, vs, ns, K, w)
 Return the `maximum(K)`-th nearest neighbors for all input points `vs`,
 with indices `ns` in original data, while respecting the theiler window `w`.
 
 This function is nothing more than a convinience call to `Neighborhood.bulksearch`.
-"""
+
+It is internal, convenience function.
+=#
 function all_neighbors(vtree, vs, ns, K, w)
     w ≥ length(vtree.data)-1 && error("Theiler window larger than the entire data span!")
     k = maximum(K)
     tw = Theiler(w, ns)
     idxs, dists = bulksearch(vtree, vs, NeighborNumber(k), tw)
+end
+
+"""
+    all_neighbors(A::Dataset, stype, w) → idxs, dists
+Find all neighbors of all points in `A` with search type `stype` (either
+[`NeighborNumber`](@ref) or [`WithinRange`](@ref)) and `w` the [Theiler window](@ref).
+
+This function is nothing more than a convinience call to `Neighborhood.bulksearch`.
+"""
+function all_neighbors(A::AbstractDataset, stype, w::Int)
+    theiler = Theiler(w)
+    tree = KDTree(A)
+    idxs, dists = bulksearch(tree, A, stype, theiler)
 end
 
 #####################################################################################
