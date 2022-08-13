15294308e265a844b912f47423f37ecfe88249d9
Datseris <datseris.george@gmail.com>
2022-08-13 17:09:11 +0200

    clarify a bit

--- src/datasets/dataset_distance.jl
+++ src/datasets/dataset_distance.jl
@@ -4,14 +4,13 @@ Calculate a distance between two `AbstractDatasets` `d1, d2`,
 i.e., a distance defined between sets of points, as dictated by `method`.
 
 If `method isa Metric` from Distances.jl, then the distance is the minimum
-distance of the pointwise distances between the points of the two datasets.
+distance of distances between the points of the two datasets.
 """
 function dataset_distance(d1::AbstractDataset, d2, metric::Metric = Euclidean())
     # this is the tree-based method
     # TODO: Check the pairwise method
     tree = KDTree(d2, metric)
-    dist, idx = [Inf], [0]
-    ε = Inf
+    dist, idx, ε = [Inf], [0], Inf
     for p in d1 # iterate over all points of attractor
         Neighborhood.NearestNeighbors.knn_point!(
             tree, p, false, dist, idx, Neighborhood.alwaysfalse
