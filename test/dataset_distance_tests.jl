07b8780634e3dc9f102afca5c0c35a0625809d3c
Datseris <datseris.george@gmail.com>
2022-08-14 18:50:45 +0200

    add a test with different metric

--- test/dataset_distance_tests.jl
+++ test/dataset_distance_tests.jl
@@ -6,5 +6,11 @@ println("\nTesting Dataset distance...")
     d1 = range(1, 10; length = 11)
     d2 = d1 .+ 10
     @test dataset_distance(Dataset(d1), Dataset(d2)) == 1.0
-    @test dataset_distance(Dataset(d1), Dataset(d2), Hausdorff(Euclidean())) == 10.0
+    @test dataset_distance(Dataset(d1), Dataset(d2), Hausdorff()) == 10.0
+
+    d1 = Dataset([SVector(0, 1)])
+    d2 = Dataset([SVector(1, 2)])
+    @test dataset_distance(d1, d2, Chebyshev()) == 1.0
+    d2 = Dataset([SVector(1, 2), SVector(1, 3)])
+    @test dataset_distance(d1, d2, Hausdorff(Chebyshev())) == 2.0
 end
