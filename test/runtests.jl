b550c7391c8148b9acb160544bcccdce22b3743f
Datseris <datseris.george@gmail.com>
2023-08-02 19:16:16 +0100

    rename to separated in tests as well

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,6 +9,10 @@ end
 defaultname(file) = uppercasefirst(replace(splitext(basename(file))[1], '_' => ' '))
 
 # Download some test timeseries
+# TODO: This is a bad idea, we should download timeseries,
+# and we should test for exact results anyways. We should test for **logically expected**
+# results, and also test for **analytically exact** results.
+
 import Downloads
 tsfolder = joinpath(@__DIR__, "timeseries")
 todownload1 = ["$n.csv" for n in 1:4]
@@ -22,14 +26,14 @@ end
 
 @testset "DelayEmbeddings tests" begin
     testfile("embedding_tests.jl")
-    testfile("traditional/delaytime_test.jl")
-    testfile("traditional/embedding_dimension_test.jl")
+    testfile("separated/delaytime_test.jl")
+    testfile("separated/embedding_dimension_test.jl")
     # TODO: All of these tests need to be re-written to be "good" tests,
     # and not just test the output the functions have had in the past in some
     # pre-existing data.
-    # include("unified/test_pecora.jl")
-    # include("unified/uzal_cost_test.jl")
-    # include("unified/test_pecuzal_embedding.jl")
-    # include("unified/test_garcia.jl")
-    # include("unified/mdop_tests.jl")
+    testfile("unified/test_pecora.jl")
+    testfile("unified/uzal_cost_test.jl")
+    testfile("unified/test_pecuzal_embedding.jl")
+    testfile("unified/test_garcia.jl")
+    testfile("unified/mdop_tests.jl")
 end
